e4c17b60c11fef7a9790706b7c98612369252e04
Rafał Nazarko <rafal.nazarko@gmail.com>
2020-12-12 15:05:56 +0100

    initial commit

---
+++ projekt/MyForm.h
@@ -0,0 +1,78 @@
+#pragma once
+
+namespace projekt {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Podsumowanie informacji o MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: W tym miejscu dodaj kod konstruktora
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Wyczyœæ wszystkie u¿ywane zasoby.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Wymagana zmienna projektanta.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Metoda wymagana do obs³ugi projektanta — nie nale¿y modyfikowaæ
+		/// jej zawartoœci w edytorze kodu.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
